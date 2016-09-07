0d0224c25829dbe945335f00b4ac031eafc5f1cc
Mo'sTec <mos_tec@outlook.com>
2016-09-07 15:51:32 +0800

    sd

--- QiniuTokenImage.php
+++ QiniuTokenImage.php
@@ -7,7 +7,7 @@ $accessKey = '15N9SEyTMq02s5Pc6aiGw88CVt0hHiWyJwni-h8w';
 $secretKey = '3N16T8zKi4vwlvFcYrt4UkpgL4ZdIsQcbA_w5bsV';
 $auth = new Auth($accessKey, $secretKey);
 
-$bucket = 'll-upload';
+$bucket = 'llhead';
 $upToken = $auth->uploadToken($bucket);
 
 $result = array('uploadToken' => $upToken);
