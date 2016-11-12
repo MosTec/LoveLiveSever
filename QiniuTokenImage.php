27c4a8d65f274014c00c87d732d9280364657209
Mo'sTec <mos_tec@outlook.com>
2016-11-12 21:45:11 +0800

    new

--- QiniuTokenImage.php
+++ QiniuTokenImage.php
@@ -3,6 +3,8 @@ require 'vendor/autoload.php';
 
 use Qiniu\Auth;
 
+header('Access-Control-Allow-Origin:*');
+
 $accessKey = '15N9SEyTMq02s5Pc6aiGw88CVt0hHiWyJwni-h8w';
 $secretKey = '3N16T8zKi4vwlvFcYrt4UkpgL4ZdIsQcbA_w5bsV';
 $auth = new Auth($accessKey, $secretKey);
